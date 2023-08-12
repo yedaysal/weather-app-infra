35d114ff2a270ae35de468768d79286ae5459a57
Yunus Emre Daysal <yunusemredaysal@gmail.com>
2023-08-12 23:35:47 +0300

    Update GCE instance output

--- terraform/gcp/gce/outputs.tf
+++ terraform/gcp/gce/outputs.tf
@@ -1,4 +1,4 @@
-output "devops-server_public_ip" {
-  description = "IP address of the created devops-server GCE instance"
-  value = google_compute_instance.devops-server.network_interface.0.access_config.0.nat_ip
+output "gce_instance_public_ip" {
+  description = "IP address of the created weather app GCE instance"
+  value = google_compute_instance.weather_app.network_interface.0.access_config.0.nat_ip
 }
