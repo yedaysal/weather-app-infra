12d66c2660e0fb94c09112cadf67efa1670deabf
Yunus Emre Daysal <yunusemredaysal@gmail.com>
2023-08-13 01:16:36 +0300

    Use zone instead of region for gke cluster location

--- terraform/gcp/gke/main.tf
+++ terraform/gcp/gke/main.tf
@@ -3,7 +3,7 @@
 
 resource "google_container_cluster" "primary" {
   name     = "${var.project_id}-gke"
-  location = var.region
+  location = var.zone
 
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
@@ -18,7 +18,7 @@ resource "google_container_cluster" "primary" {
 # Separately Managed Node Pool
 resource "google_container_node_pool" "primary_nodes" {
   name       = google_container_cluster.primary.name
-  location   = var.region
+  location   = var.zone
   cluster    = google_container_cluster.primary.name
   
   version = data.google_container_engine_versions.gke_version.release_channel_latest_version["STABLE"]
--- terraform/gcp/gke/variables.tf
+++ terraform/gcp/gke/variables.tf
@@ -12,3 +12,8 @@ variable "region" {
   type    = string
   default = "europe-central2"
 }
+
+variable "zone" {
+  type    = string
+  default = "europe-central2-a"
+}
